2c6d787c4c66d4f336bce4111a71368511025915
Alexandre Haddad-Delaveau <ahaddadd@uwaterloo.ca>
2025-09-18 19:53:43 -0400

    Completed costmap

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,23 +1,26 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
- 
+
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
- 
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
 #include "costmap_core.hpp"
- 
-class CostmapNode : public rclcpp::Node {
-  public:
-    CostmapNode();
-    
-    // Place callback function here
-    void publishMessage();
- 
-  private:
-    robot::CostmapCore costmap_;
-    // Place these constructs here
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
+
+class CostmapNode : public rclcpp::Node
+{
+public:
+  CostmapNode();
+
+private:
+  std::vector<std::vector<int>> costmap_;
+  robot::CostmapCore costmap_core_;
+
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr occ_grid_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+
+  void publishCostmap(int width, int origin, float resolution);
+  void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
 };
- 
-#endif 
+
+#endif
